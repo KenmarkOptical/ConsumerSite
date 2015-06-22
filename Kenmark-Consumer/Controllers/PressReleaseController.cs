ad424c9778c2ce3be19b153ff4205409be378041
travistme <travistme@gmail.com>
2015-06-22 16:17:23 -0400

    End of day commit 6/22 - Updated nav bar dropdowns, started Where to buy page (added in map with pin start location at kenmark), started press release page (got layout ready to go for insertion of data.

---
+++ Kenmark-Consumer/Controllers/PressReleaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kenmark_Consumer.Controllers
+{
+    public class PressReleaseController : Controller
+    {
+        //
+        // GET: /PressRelease/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
