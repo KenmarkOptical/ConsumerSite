db789f9d2bfac26ad0fc20539593dc0a494a9c29
mscard02 <mscard02@gmail.com>
2015-07-06 10:23:43 -0400

    added cookie data for max mind geolocation and press release data class

---
+++ Kenmark-Consumer/Models/PressRelease.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kenmark_Consumer.Models
+{
+    public class PressRelease
+    {
+        List<press_releases> Items { get; set; }
+
+        public PressRelease GetItems()
+        {
+            PressRelease pr = new PressRelease();
+            pr.Items = new List<press_releases>();
+
+            using (KenmarkTestDBEntities db = new KenmarkTestDBEntities())
+            {
+                pr.Items = db.press_releases.Where(m => m.active == true).ToList();
+            }
+
+            return pr;
+        }
+    }
+}
