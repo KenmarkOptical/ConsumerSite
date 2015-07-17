e9e7355f4edd067143258cda824b061730b04780
travistme <travistme@gmail.com>
2015-07-17 10:33:46 -0400

    Style controller push

---
+++ Kenmark-Consumer/Controllers/StyleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kenmark_Consumer.Controllers
+{
+    public class StyleController : Controller
+    {
+        //
+        // GET: /Style/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
