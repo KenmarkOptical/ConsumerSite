7ce7dd14dcc4b5e7311244eee11e19b30e424620
travistme <travistme@gmail.com>
2015-07-29 16:24:07 -0400

    getting CMS ready to be started

---
+++ Kenmark-Consumer/Controllers/TestCMSController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kenmark_Consumer.Controllers
+{
+    public class TestCMSController : Controller
+    {
+        //
+        // GET: /TestCMS/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateContent(String title, String date, String story, String storyImage)
+        {
+            return View();
+        }
+
+    }
+}
