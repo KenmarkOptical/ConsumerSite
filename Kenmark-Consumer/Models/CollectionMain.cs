77fb4ccf332e8b17100eee48d1787f6b01b61a4b
mscard02 <mscard02@gmail.com>
2015-08-25 14:56:01 -0400

    fixed collection error

--- Kenmark-Consumer/Models/CollectionMain.cs
+++ Kenmark-Consumer/Models/CollectionMain.cs
@@ -14,6 +14,7 @@ namespace Kenmark_Consumer.Models
 
         public CollectionMain GetMain(string collection)
         {
+            collection = collection.Replace("-", " ");
             CollectionMain c = new CollectionMain();
             using (KenmarkTestDBEntities db = new KenmarkTestDBEntities())
             {
