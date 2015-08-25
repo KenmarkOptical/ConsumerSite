126c0f5434c89b2622628601e1f59bac909b2bb5
mscard02 <mscard02@gmail.com>
2015-08-25 11:40:07 -0400

    added logic for shop menu

--- Kenmark-Consumer/CMS_Like_Collection.cs
+++ Kenmark-Consumer/CMS_Like_Collection.cs
@@ -10,11 +10,15 @@
 namespace Kenmark_Consumer
 {
     using System;
+    using System.Collections.Generic;
     
-    public partial class usp_get_statementEmailList_Result
+    public partial class CMS_Like_Collection
     {
-        public string contact_name { get; set; }
-        public string acc_number { get; set; }
-        public string email { get; set; }
+        public int id { get; set; }
+        public int like_id { get; set; }
+        public string icon_image { get; set; }
+        public string icon_hover { get; set; }
+        public string about { get; set; }
+        public string about_image { get; set; }
     }
 }
---
+++ Kenmark-Consumer/Models/ShopMenu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kenmark_Consumer.Models
+{
+    public class ShopMenu
+    {
+        List<ShopItem> Items = new List<ShopItem>();
+
+        public ShopMenu GetItems()
+        {
+            ShopMenu m = new ShopMenu();
+            
+            using (KenmarkTestDBEntities db = new KenmarkTestDBEntities())
+            {
+                m.Items = (from c in db.CMS_Like_Collection
+                           join l in db.Kenmark_Collections_like on c.like_id equals l.ID
+                           select new ShopItem
+                           {
+                               id = c.id,
+                               like_id = c.like_id,
+                               icon_image = c.icon_image,
+                               icon_hover = c.icon_hover,
+                               about = c.about,
+                               about_image = c.about_image,
+                               collection = l.Collecton,
+                               site_display = l.Site_Display,
+                               @group = l.Group,
+                               type = l.Type,
+                               order = l.Order,
+                               enabled = l.Enabled
+                           }).ToList();
+            }
+
+            return m;
+        }
+    }
+
+    public class ShopItem
+    {
+        public int id { get; set; }
+        public int like_id { get; set; }
+        public string icon_image { get; set; }
+        public string icon_hover { get; set; }
+        public string about { get; set; }
+        public string about_image { get; set; }
+        public string collection { get; set; }
+        public string site_display { get; set; }
+        public string group { get; set; }
+        public string type { get; set; }
+        public int? order { get; set; }
+        public bool? enabled { get; set; }
+    }
+}
