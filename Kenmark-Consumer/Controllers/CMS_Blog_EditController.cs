b776b6f050be27dd432e45b423cdf38abbc8cbe3
travistme <travistme@gmail.com>
2015-09-11 15:54:01 -0400

    end of day commit 9/11
    Updated "Our Story" page and the profile dropdown with new images.
    
    Working on "Edit" part of CMS

--- Kenmark-Consumer/Controllers/CMS_Blog_EditController.cs
+++ Kenmark-Consumer/Controllers/CMS_Blog_EditController.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace Kenmark_Consumer.Controllers
 {
-    public class CMS_Blog_EditController : Controller
+    public class CMS_Blog_EditController : MyBaseController
     {
         //
         // GET: /CMS_Blog_Edit/
@@ -18,16 +19,23 @@ namespace Kenmark_Consumer.Controllers
             return View(b);
         }
 
-        [HttpPost]
+        
         public ActionResult GetBlog(int id)
         {
+
+            
             CMS_Blog a = new CMS_Blog();
             SingleBlog b = new SingleBlog();
+            b = a.GetBlog(id);
 
-           b = a.GetBlog(id);
-
-           return Json(new { data = b, }, JsonRequestBehavior.AllowGet);
+            var serializer = new JavaScriptSerializer();
+            serializer.MaxJsonLength = Int32.MaxValue;
+            var resultData = b.data;
+            string html = RenderPartialViewToString("_Grid", b);
+            return Json(new { html = html, blogData = serializer.Serialize(resultData) }, JsonRequestBehavior.AllowGet);
         }
+
+       
 
         [HttpPost]
         public ActionResult EditBlog(SingleBlog blog)
